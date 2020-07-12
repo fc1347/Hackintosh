f6fec1dcad7a81ae66b97c074a0213f4dccdb65f
Paul-Louis Charrière <plcharriere@gmail.com>
2020-07-12 14:38:54 +0200

    apps install order + dock add order

--- Apps/Install.sh
+++ Apps/Install.sh
@@ -7,6 +7,8 @@ rm ~/.bash_profile
 
 # Remove current apps
 
+echo "Killing and removing current apps..."
+
 kill -9 $(pgrep Chrome) 2&>/dev/null
 kill -9 $(pgrep Spotify) 2&>/dev/null
 kill -9 $(pgrep Discord) 2&>/dev/null
@@ -31,6 +33,16 @@ rm -rf /Applications/The\ Unarchiver.app
 rm -rf /Applications/Docker.app
 rm -rf /Applications/Spotify.app
 
+echo "Configuring Dock..."
+
+defaults delete com.apple.dock; killall Dock
+sleep 5
+defaults delete com.apple.dock persistent-apps; killall Dock
+sleep 5
+defaults write com.apple.dock show-recents -bool FALSE; killall Dock
+sleep 5
+defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Launchpad.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
+
 # Google Chrome
 
 echo "Downloading and installing Google Chrome..."
@@ -41,40 +53,7 @@ hdiutil attach ./dl/googlechrome.dmg -quiet
 cp -r /Volumes/Google\ Chrome/Google\ Chrome.app /Applications
 hdiutil detach /Volumes/Google\ Chrome -quiet
 
-
-# VSCode
-
-echo "Downloading and installing VSCode..."
-
-curl -L https://go.microsoft.com/fwlink/?LinkID=620882 -o ./dl/VSCode-darwin-stable.zip 1&>/dev/null
-
-unzip -qq ./vscode/VSCode-darwin-stable.zip -d /Applications/
-
-mkdir -p ~/Library/Application\ Support/Code/User
-cp ./vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-
-cat << EOF >> ~/.bash_profile
-export PATH="\$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
-EOF
-
-# iTerm2
-
-echo "Downloading and installing iTerm2..."
-
-curl -L https://iterm2.com/downloads/stable/latest -o ./dl/iTerm2.zip 1&>/dev/null
-
-unzip -qq ./dl/iTerm2.zip -d /Applications
-
-# Rectangle
-
-echo "Downloading and installing Rectangle..."
-
-curl -L -o ./dl/Rectangle.dmg $(curl -s https://api.github.com/repos/rxhanson/Rectangle/releases/latest | grep "browser_download_url.*dmg" | cut -d '"' -f 4) 1&>/dev/null
-
-hdiutil attach ./dl/Rectangle.dmg -quiet
-cp -r /Volumes/Rectangle*/Rectangle.app /Applications
-hdiutil detach /Volumes/Rectangle* -quiet
-open /Applications/Rectangle.app
+defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Google Chrome.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
 
 # Discord
 
@@ -86,44 +65,7 @@ hdiutil attach ./dl/Discord.dmg -quiet
 cp -r /Volumes/Discord/Discord.app /Applications
 hdiutil detach /Volumes/Discord -quiet
 
-# Steam
-
-echo "Downloading and installing Steam..."
-
-curl -L https://steamcdn-a.akamaihd.net/client/installer/steam.dmg -o ./dl/Steam.dmg 1&>/dev/null
-
-hdiutil attach ./dl/Steam.dmg -quiet
-cp -r /Volumes/Steam/Steam.app /Applications
-hdiutil detach /Volumes/Steam -quiet
-
-# eqMac2
-
-echo "Downloading and installing eqMac2..."
-
-hdiutil attach ./dmg/eqMac2.dmg -quiet
-cp -r /Volumes/eqMac2/eqMac2.app /Applications
-hdiutil detach /Volumes/eqMac2 -quiet
-open /Applications/eqMac2.app
-
-# The Unarchiver
-
-echo "Downloading and installing The Unarchiver..."
-
-curl -L https://dl.devmate.com/com.macpaw.site.theunarchiver/TheUnarchiver.dmg -o ./dl/TheUnarchiver.dmg 1&>/dev/null
-
-hdiutil attach ./dl/TheUnarchiver.dmg -quiet
-cp -r /Volumes/The\ Unarchiver/The\ Unarchiver.app /Applications
-hdiutil detach /Volumes/The\ Unarchiver -quiet
-
-# Docker
-
-echo "Downloading and installing Docker..."
-
-curl -L https://download.docker.com/mac/stable/Docker.dmg -o ./dl/Docker.dmg 1&>/dev/null
-
-hdiutil attach ./dl/Docker.dmg -quiet
-cp -r /Volumes/Docker/Docker.app /Applications
-hdiutil detach /Volumes/Docker -quiet
+defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Discord.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
 
 # Slack
 
@@ -134,6 +76,8 @@ curl -L https://slack.com/ssb/download-osx -o ./dl/Slack.dmg 1&>/dev/null
 hdiutil attach ./dl/Slack.dmg -quiet
 cp -r /Volumes/Slack.app/Slack.app /Applications
 hdiutil detach /Volumes/Slack.app -quiet
+
+defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Slack.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
 
 # Spotify
 
@@ -150,6 +94,85 @@ while [[ $(pgrep Install\ Spotify) ]]
 do
 sleep 1
 done
+
+defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Spotify.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
+
+# VSCode
+
+echo "Downloading and installing VSCode..."
+
+curl -L https://go.microsoft.com/fwlink/?LinkID=620882 -o ./dl/VSCode-darwin-stable.zip 1&>/dev/null
+
+unzip -qq ./vscode/VSCode-darwin-stable.zip -d /Applications/
+
+mkdir -p ~/Library/Application\ Support/Code/User
+cp ./vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+
+cat << EOF >> ~/.bash_profile
+export PATH="\$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+EOF
+
+defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Visual Studio Code.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
+
+# iTerm2
+
+echo "Downloading and installing iTerm2..."
+
+curl -L https://iterm2.com/downloads/stable/latest -o ./dl/iTerm2.zip 1&>/dev/null
+
+unzip -qq ./dl/iTerm2.zip -d /Applications
+
+defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/iTerm.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
+
+# Steam
+
+echo "Downloading and installing Steam..."
+
+curl -L https://steamcdn-a.akamaihd.net/client/installer/steam.dmg -o ./dl/Steam.dmg 1&>/dev/null
+
+hdiutil attach ./dl/Steam.dmg -quiet
+cp -r /Volumes/Steam/Steam.app /Applications
+hdiutil detach /Volumes/Steam -quiet
+
+# Rectangle
+
+echo "Downloading and installing Rectangle..."
+
+curl -L -o ./dl/Rectangle.dmg $(curl -s https://api.github.com/repos/rxhanson/Rectangle/releases/latest | grep "browser_download_url.*dmg" | cut -d '"' -f 4) 1&>/dev/null
+
+hdiutil attach ./dl/Rectangle.dmg -quiet
+cp -r /Volumes/Rectangle*/Rectangle.app /Applications
+hdiutil detach /Volumes/Rectangle* -quiet
+open /Applications/Rectangle.app
+
+# eqMac2
+
+echo "Downloading and installing eqMac2..."
+
+hdiutil attach ./dmg/eqMac2.dmg -quiet
+cp -r /Volumes/eqMac2/eqMac2.app /Applications
+hdiutil detach /Volumes/eqMac2 -quiet
+open /Applications/eqMac2.app
+
+# Docker
+
+echo "Downloading and installing Docker..."
+
+curl -L https://download.docker.com/mac/stable/Docker.dmg -o ./dl/Docker.dmg 1&>/dev/null
+
+hdiutil attach ./dl/Docker.dmg -quiet
+cp -r /Volumes/Docker/Docker.app /Applications
+hdiutil detach /Volumes/Docker -quiet
+
+# The Unarchiver
+
+echo "Downloading and installing The Unarchiver..."
+
+curl -L https://dl.devmate.com/com.macpaw.site.theunarchiver/TheUnarchiver.dmg -o ./dl/TheUnarchiver.dmg 1&>/dev/null
+
+hdiutil attach ./dl/TheUnarchiver.dmg -quiet
+cp -r /Volumes/The\ Unarchiver/The\ Unarchiver.app /Applications
+hdiutil detach /Volumes/The\ Unarchiver -quiet
 
 # Nodejs
 
@@ -189,24 +212,8 @@ EOF
 
 # Add apps do Dock
 
-echo "Configuring Dock..."
+echo "Restarting Dock..."
 
-defaults delete com.apple.dock; killall Dock
-sleep 5
-defaults delete com.apple.dock persistent-apps; killall Dock
-sleep 5
-defaults write com.apple.dock show-recents -bool FALSE; killall Dock
-sleep 5
-
-defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Launchpad.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
-defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Google Chrome.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
-defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Discord.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
-defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Slack.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
-defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Spotify.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
-defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Visual Studio Code.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
-defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/iTerm.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
-defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/System Preferences.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
 killall Dock
-
 
 rm -rf ./dl
